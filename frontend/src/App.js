243f157224f7d2ac76f830369be744a68a6766f2
kyleronayne <kyleronaynep@gmail.com>
2020-11-10 01:55:28 -0500

    Created initial app.js state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,40 @@
+import React, { Component } from 'react';
 import './App.css';
+import Tickets from './components/Tickets';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    tickets: [
+      {
+        id: 1,
+        owner: "Tom",
+        summary: "Remove HDD from PC 2",
+        description: "",
+        timestamp: "",
+        priority: "",
+        assignee: "",
+        status: false
+      },
+      {
+        id: 2,
+        owner: "Roger",
+        summary: "Create Disk Drive Spreadsheet",
+        description: "",
+        timestamp: "",
+        priority: "",
+        assignee: "",
+        status: false
+      }
+    ]
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <Tickets></Tickets>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ frontend/src/components/Tickets.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Tickets extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Tickets</h1>
+            </div>
+        );
+    }
+}
+
+export default Tickets;
